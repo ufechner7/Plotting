d5d7d8d2174545d4133a782366297d9a7bd34f0a
Uwe Fechner <fechner@aenarete.eu>
2022-10-01 16:35:51 +0200

    Add trace dialog

--- src/plot2.jl
+++ src/plot2.jl
@@ -1,4 +1,5 @@
 # example for using plotting in Julia with two lines in one window
+# in addition it shows the trace dialog, which allows to toggle the visibility of the lines
 using Plots
 
 include("plot_helpers.jl")
@@ -10,6 +11,9 @@ x  = 0:0.01:100
 y1 = sin.(x)
 y2 = cos.(x)
 
-plot(x, y1, label="sin", legend=true)
+p = plot(x, y1, label="sin", legend=true)
 plot!(x, y2, label="cos", legend=true, color=:red)
+sleep(0.1)
+InspectDR.tracedialog_show(p)
+println("You must close the trace dialog before you can zoom!")
 nothing
