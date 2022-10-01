f34911a95293cc5f5da925733aa8da4445b826be
Uwe Fechner <fechner@aenarete.eu>
2022-10-01 15:08:31 +0200

    Add script two_plots.jl

---
+++ src/two_plots.jl
@@ -0,0 +1,15 @@
+# example for using plotting in Julia
+using Plots
+
+include("plot_helpers.jl")
+
+# main
+help()
+
+x  = 0:0.01:100
+y1 = sin.(x)
+y2 = cos.(x)
+
+plot(x, y1, legend=false)
+plot(x, y2, legend=false, color=:red)
+nothing
